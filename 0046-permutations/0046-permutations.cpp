afaa18d2f0bd0ba9bd567697c24b38003e7b9045
Dev Dua <147393008+devdua-04@users.noreply.github.com>
2025-01-15 19:06:44 +0530

    Time: 3 ms (28.72%), Space: 12.1 MB (11.84%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void solution(vector<int>& nums,vector<vector<int>> &sol,vector<int> temp,int n){
+        if(temp.size()==n){
+            sol.push_back(temp);
+            return;
+        }
+        else{
+            for(int j=0;j<nums.size();j++){
+                    int t=nums[j];
+                    temp.push_back(t);
+                    nums.erase(nums.begin()+j);
+                    solution(nums,sol,temp,n);
+                    temp.pop_back();
+                    nums.insert(nums.begin()+j,t);
+                    // temp.pop_back();
+                }
+            } 
+            return;
+        }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> sol;
+        vector<int> temp;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int t=nums[i];
+            temp.push_back(t);
+            nums.erase(nums.begin()+i);
+            solution(nums,sol,temp,n);
+            nums.insert(nums.begin()+i,t);
+            temp.pop_back();
+        }
+        return sol;
+    }
+};
