d18a230b0fc143e6186771b162a34368b2382e8a
Dev Dua <147393008+devdua-04@users.noreply.github.com>
2025-01-15 19:19:53 +0530

    Time: 219 ms (7.54%), Space: 74.1 MB (5.09%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void solution(vector<int>& nums, set<vector<int>>& sol, vector<int> temp,int n) {
+        if (temp.size() == n) {
+            sol.insert(temp);
+            return;
+        } else {
+            for (int j = 0; j < nums.size(); j++) {
+                    int t = nums[j];
+                    temp.push_back(t);
+                    nums.erase(nums.begin() + j);
+                    solution(nums, sol, temp, n);
+                    temp.pop_back();
+                    nums.insert(nums.begin() + j, t);
+                    // temp.pop_back();
+            }
+        }
+        return;
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<int> temp;
+        set<vector<int>> sol;
+        int n=nums.size();
+        for (int i = 0; i < n; i++) {
+            int t = nums[i];
+            temp.push_back(t);
+            nums.erase(nums.begin() + i);
+            solution(nums, sol, temp, n);
+            nums.insert(nums.begin() + i, t);
+            temp.pop_back();
+        }
+        vector<vector<int>> s;
+        s.assign(sol.begin(), sol.end());
+        return s;
+    }
+};
