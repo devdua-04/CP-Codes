166fd88e6a5dda823ade163c39e5e4fa217626fe
Dev Dua <147393008+devdua-04@users.noreply.github.com>
2025-01-30 23:03:35 +0530

    Time: 0 ms (100%), Space: 8.1 MB (81.08%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0;i<s.length();i++){
+            if(int(s[i])>=65 && int(s[i])<=90){
+                s[i]=char(s[i]+32);
+            }
+        }
+        return s;
+    }
+};
