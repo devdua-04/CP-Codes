8dc8b98cd6f4fc7edc0913ba034633f1ba03cceb
Dev Dua <147393008+devdua-04@users.noreply.github.com>
2025-02-12 13:36:23 +0530

    Time: 64 ms (7.67%), Space: 166.5 MB (5.54%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -3,7 +3,7 @@ public:
     string reverseWords(string s) {
         vector<string> sol;
         string temp="";
-        for(int i=0;i<s.length();i++){
+        for(int i=s.length()-1;i>-1;i--){
             if(s[i]==' '){
                 if(temp.length()!=0){
                     sol.push_back(temp);
@@ -17,12 +17,7 @@ public:
             sol.push_back(temp);
         string res="";
         for(string k:sol){
-            for(int i=0;i<k.length()/2;i++){
-                char t=k[i];
-                k[i]=k[k.length()-1-i];
-                k[k.length()-1-i]=t;
-            }
-            res=res+k+" ";
+            res=k+" "+res;
         }
         res.pop_back();
         return res;
