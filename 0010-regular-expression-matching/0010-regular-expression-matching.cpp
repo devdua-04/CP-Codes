6ff155c054dd4f7d5a4ced465e9cf06967d56b03
Dev Dua <147393008+devdua-04@users.noreply.github.com>
2025-06-08 17:34:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        if (p.empty()) return s.empty();
-        
-        bool first_match = (!s.empty() &&
-            (s[0] == p[0] || p[0] == '.'));
+        int i = 0, j = 0;
+        int star_i = -1, star_j = -1;
 
-        if (p.length() >= 2 && p[1] == '*') {
-            return (isMatch(s, p.substr(2)) ||
-                    (first_match && isMatch(s.substr(1), p)));
-        } else {
-            return first_match && isMatch(s.substr(1), p.substr(1));
+        while (i < s.length()) {
+            if (j < p.length() && (p[j] == s[i] || p[j] == '.')) {
+                // Characters match, move forward
+                i++;
+                j++;
+            }
+            else if (j + 1 < p.length() && p[j + 1] == '*') {
+                // Store position and skip `char*`
+                star_i = i;
+                star_j = j;
+                j += 2;
+            }
+            else if (star_j != -1) {
+                // Backtrack: try to match one more character via previous `char*`
+                star_i++;
+                i = star_i;
+                j = star_j + 2;
+            }
+            else {
+                return false;
+            }
         }
+
+        // Check for remaining pattern: should be all `x*`
+        while (j + 1 < p.length() && p[j + 1] == '*') {
+            j += 2;
+        }
+
+        return j == p.length();
     }
 };
